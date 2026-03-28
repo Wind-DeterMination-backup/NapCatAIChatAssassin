56356898605b6fa65407d83be23eb9263a3ae933
仑质 <benzenpenxil@foxmail.com>
2026-03-28 17:12:52 +0800

    引入知识缓存的推荐算法实现

--- OlivOSAIChatAssassin/main.py
+++ OlivOSAIChatAssassin/main.py
@@ -365,11 +365,14 @@ def reply_to_group(plugin_event, group_id):
         if type(thisMemoryM) is dict:
             for k, v in thisMemoryM.items():
                 flagHit = False
+                rank = None
                 for j in history:
-                    if k in j.get('message', ''):
+                    rank = get_recommendRank(k, j.get('message', ''))
+                    if get_recommendMatch(rank):
                         flagHit = True
+                        break
                 if flagHit:
-                    log(f'PEAK UP - [{key_gMemory}] {k}')
+                    log(f'PEAK UP - [{key_gMemory}] {k} ({rank})')
                     thisMemoryG[key_gMemory_const][k] = v
 
     key_gMemory_const = '人物关系'
@@ -742,4 +745,78 @@ def get_gGroupKnowledgeCounter(group_id: str, flag_busy: bool, rate: int = 4):
         log(f'KNOWLEDGE [{group_id}] - {gGroupKnowledgeCounter[group_id]} / {gGroupKnowledgeCounterLimit}')
     else:
         log(f'KNOWLEDGE [{group_id}] - HIT')
+    return res
+
+
+def get_recommendRank(word1_in: str, word2_in: str, gate_rank: int = 1000, rate: float = 0.1):
+    iRank = 1
+    find_flag = 1
+    word1 = word1_in.lower()
+    word2 = word2_in.lower()
+    if not word1 or not word2:
+        return gate_rank + 1  # 返回一个高数值，表示完全不匹配，因为有些键值可能因为误设置从而为空字符串
+    # word1 为短字符串，此场景不进行对调
+    # if len(word1) > len(word2):
+    #     [word1, word2] = [word2, word1]
+    if len(word1) > len(word2):
+        return gate_rank + 2
+    word1_len = len(word1)
+    word2_len = len(word2)
+
+    if word2.find(word1) != -1:
+        find_flag = 0
+
+    # LCS
+    dp1 = []
+    dp1_first = [0]
+    for word1_this in word1:
+        dp1_first.append(0)
+    dp1.append(dp1_first)
+    for word2_this in word2:
+        dp1.append([0] + [0] * word1_len)
+    tmp_i_list = range(1, word1_len + 1)
+    tmp_j_list = range(1, word2_len + 1)
+    for i in tmp_i_list:
+        for j in tmp_j_list:
+            if word1[i - 1] == word2[j - 1]:
+                dp1[j][i] = dp1[j - 1][i - 1] + 1
+            else:
+                dp1[j][i] = max(dp1[j - 1][i], dp1[j][i - 1])
+    iRank_1 = dp1[word2_len][word1_len]
+
+    # minDistance
+    dp2 = []
+    dp2_first = [0]
+    tmp_counter = 1
+    for word1_this in word1:
+        dp2_first.append(tmp_counter)
+        tmp_counter += 1
+    dp2.append(dp2_first)
+    tmp_counter = 1
+    for word2_this in word2:
+        dp2.append([tmp_counter] + [0] * word1_len)
+        tmp_counter += 1
+    tmp_i_list = range(1, word1_len + 1)
+    tmp_j_list = range(1, word2_len + 1)
+    for i in tmp_i_list:
+        for j in tmp_j_list:
+            if word1[i - 1] == word2[j - 1]:
+                dp2[j][i] = dp2[j - 1][i - 1]
+            else:
+                dp2[j][i] = min(dp2[j - 1][i - 1], min(dp2[j - 1][i], dp2[j][i - 1])) + 1
+    iRank_2 = dp2[word2_len][word1_len]
+
+    iRank = (find_flag) * (word2_len * (word1_len - iRank_1) + iRank_2 + 1)
+    iRank = int(int((iRank * iRank) / word1_len) / word2_len)
+
+    if iRank >= int(word1_len * word2_len * rate):
+        iRank += gate_rank
+
+    return iRank
+
+
+def get_recommendMatch(rank, gate_rank: int = 1000):
+    res = False
+    if rank < gate_rank:
+        res = True
     return res
